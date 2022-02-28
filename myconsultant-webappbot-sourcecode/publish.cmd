2a79bb48701255ea284fd70567321834d9f658dc
Naga Leela Krishna Rajamahendravarapu <65774883+leelakrishna288@users.noreply.github.com>
2022-02-28 18:48:55 +0530

    Delete myconsultant-webappbot-sourcecode directory

--- myconsultant-webappbot-sourcecode/publish.cmd
+++
@@ -1,3 +0,0 @@
-nuget restore
-msbuild QnABot.sln -p:DeployOnBuild=true -p:PublishProfile=.\myconsultant-webappbot-Web-Deploy.pubxml -p:Password=8Jy3wpcr30DsaR1A24w8883t9Kk3gvYsHWTBqoqlSNgyvkgpNihPfyxmye0E
-
