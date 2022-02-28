c531f60af48f08a0ab3553c9a07056a3c78697df
Naga Leela Krishna Rajamahendravarapu <65774883+leelakrishna288@users.noreply.github.com>
2022-02-28 16:42:30 +0530

    Add files via upload

---
+++ myconsultant-webappbot-sourcecode/publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild QnABot.sln -p:DeployOnBuild=true -p:PublishProfile=.\myconsultant-webappbot-Web-Deploy.pubxml -p:Password=8Jy3wpcr30DsaR1A24w8883t9Kk3gvYsHWTBqoqlSNgyvkgpNihPfyxmye0E
+
